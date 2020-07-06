e1d817acad6fa847034fb74c7e8d692d09661f2d
Edwin Chong <edwin.chong@siemens.com>
2020-07-06 07:17:54 -0400

    complete 1.3

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100
+total_bounces = 10
+bounce = 0
+
+while bounce < total_bounces:
+    height = height * (3/5)
+    bounce = bounce + 1
+    print(bounce, round(height, 4))
+
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,29 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+extra_payment_start_month = 60
+extra_payment_end_month = 108
+extra_payment_amount = 1000
+month = 0
+
+while principal > 0:
+    month += 1
+    current_payment = payment
+    if month >= extra_payment_start_month and month < extra_payment_end_month:
+        current_payment += extra_payment_amount
+
+    principal = principal * (1+rate/12) - current_payment
+    if principal < 0:
+        current_payment += principal
+        principal = 0
+
+    total_paid += current_payment
+
+    print(month, round(total_paid, 2), round(principal, 2))
+
+print('Total paid', round(total_paid, 2))
+print('Months', month)
